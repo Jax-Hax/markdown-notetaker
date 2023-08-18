763d13f046283ddb84de6c2b23eb1958eeefe188
Jax-Hax <jaxhacks543@gmail.com>
2023-08-18 09:06:48 -0400

    i dont know enough for this :(

--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<style>
-    h1{
-       margin: 0;
-       color: white 
-    }
-</style>
-<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 	let html = '<h1>hi</h1>';
 	let markdown = '';
-	let rules = [
-		{ regex: /(\*\*|__)(.*?)\1/g, replace: '<strong>$2</strong>' }, // Bold
-		{ regex: /(\*|_)(.*?)\1/g, replace: '<em>$2</em>' }, // Italics
-		{ regex: /# (.*?)(?:\r?\n|$)/g, replace: '<h1>$1</h1>' }, // Heading 1
-		{ regex: /## (.*?)(?:\r?\n|$)/g, replace: '<h2>$1</h2>' } // Heading 2
-		// Add more rules for other Markdown syntax
+	const rules = [
+        { regex: /(\*\*|__)(.*?)\1/g, replace: '<strong>$2</strong>' }, // Bold
+        { regex: /(\*|_)(.*?)\1/g, replace: '<em>$2</em>' }, // Italics
+        { regex: /# (.*?)(?:\r?\n|$)/g, replace: '<h1>$1</h1>' }, // Heading 1
+        { regex: /## (.*?)(?:\r?\n|$)/g, replace: '<h2>$1</h2>' }, // Heading 2
+		{ regex: /([^\n]+)(\*)([^\n]+)/g, replace: "<ul><li>$3</li></ul>" } // Numbered list
 	];
 	function renderMarkdown() {
 		html = markdown;
@@ -15,26 +15,48 @@
 		});
 	}
 </script>
-<div id="row">
-<textarea bind:value={markdown} />
-{@html html}
-</div>
-<br />
-<button on:click={renderMarkdown}>Render</button>
-<br />
-<style>
-	textarea {
-		resize: none;
-        padding: 0;
-        margin: 0;
-        width: 50%;
-        height: 100%; 
-	}
-    #row{
-        display: flex;
-        flex-direction: row;
-    }
-    h1{
-        margin: 0
-    }
-</style>
+
+<html lang="en">
+	<body>
+		<div id="row">
+			<div class="rowItem">
+				<textarea bind:value={markdown} />
+			</div>
+			<div id="divider" />
+			<div class="rowItem">
+				{@html html}
+			</div>
+		</div>
+		<br />
+		<button on:click={renderMarkdown}>Render</button>
+		<br />
+	</body>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+		}
+		textarea {
+			resize: none;
+			width: 100%;
+			padding: 1em;
+			margin: 0;
+			height: 100vh;
+			box-sizing: border-box;
+		}
+		#divider {
+			border-left: 2px solid black;
+			position: absolute;
+			left: 50%;
+			margin-left: -3px;
+			top: 0;
+		}
+		#row {
+			display: flex;
+			flex-direction: row;
+		}
+		.rowItem {
+			flex: 1;
+		}
+	</style>
+</html>
